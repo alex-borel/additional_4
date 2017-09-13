6c179e2672752abf9959ca6b8806f4abd7e7ec7d
Alexander Borel <alex.b.minsk@gmail.com>
2017-09-13 10:24:29 +0300

    additional-4/feature: implement solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+  let result = [];
+  first = first.split('').reverse();
+  second = second.split('').reverse();
+
+  first.forEach((a, i, first) => {
+    second.forEach((b, j, second) => {
+      if (!result[i + j]) {
+        result[i + j] = 0;
+      };
+      result[i + j] += first[i] * second[j];
+    });
+  });
+
+  result.forEach((a, i, result) => {
+    if (result[i] >= 10) {
+      if (!result[i + 1]) {
+        result[i + 1] = 0;
+      };
+      result[i + 1] += Math.floor(result[i] / 10);
+      result[i] = result[i] % 10;
+    };
+  });
+
+  return result.reverse().join('');
+};
+
